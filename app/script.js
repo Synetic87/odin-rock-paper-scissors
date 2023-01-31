2589b92d827b79f28b18b324ed5f31988140cc73
Synetic87 <77205748+Synetic87@users.noreply.github.com>
2023-01-31 23:46:42 +0100

    Einfaches Spiel mit Abgleich der Eingabe mit Array
    In die playRound Function noch die Abrage eingebaut
    ob das eingegebene Wort auch im Array erscheint. Wenn nicht
    wird eine Fehlermeldung in der Console gezeigt.
    
    [Ticket: X]

--- app/script.js
+++ app/script.js
@@ -5,12 +5,22 @@ console.log('Let the battle begin')
 const weapons = ["Rock" , "Paper", "Scissors"];
 
 //Der Computer wählt seinen Gegenstand - getComputerChoice
+let getComputerChoice = Math.floor(Math.random() * weapons.length);
+
+console.log(getComputerChoice, weapons[getComputerChoice])
+let computerSelection = weapons[getComputerChoice]
+
+console.log(computerSelection)
+
+
 //Der Spieler wählt seinen Gegenstand
 //Die Eingabe wird case-insensitive gemacht, erste Buchstabe immer groß.
 
-
-
-
+let playerSelection = prompt("Wähle deinen Gegenstand. Rock, Paper, Scissors?");
+let playerSelectionClean = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase()
+let playerWon = 'You won! ' + playerSelectionClean + ' beats ' + computerSelection + '!';
+let playerLose = 'You lose! ' + computerSelection + ' beats ' + playerSelectionClean + '!'
+console.log(playerSelectionClean)
 
 
 //Beider Ergebisse werden verglichen und ein Sieger bestimmt.
@@ -20,35 +30,26 @@ const weapons = ["Rock" , "Paper", "Scissors"];
 //wenn der Spieler Stein hat, und der PC Papier, gewinnt der PC.
 
 function playRound(playerSelectionClean, computerSelection){
-    getComputerChoice = Math.floor(Math.random() * weapons.length);
-    computerSelection = weapons[getComputerChoice];
-    
-    playerSelection = prompt("Wähle deinen Gegenstand. Rock, Paper, Scissors?");
-    playerSelectionClean = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
-
-    let playerWon = 'You won! ' + playerSelectionClean + ' beats ' + computerSelection + '!';
-    let playerLose = 'You lose! ' + computerSelection + ' beats ' + playerSelectionClean + '!';
-    if (playerSelectionClean == 'Rock' && computerSelection == 'Scissors') {
-        console.log(playerWon);
-    }
-    if (playerSelectionClean == 'Paper' && computerSelection == 'Rock') {
-        console.log(playerWon);
-    } 
-    if (playerSelectionClean == 'Scissors' && computerSelection == 'Paper') {
-        console.log(playerWon);
-    } 
-    else if (playerSelectionClean === computerSelection){
-        console.log("It's a tie!")
-    }
-    else {
-        console.log(playerLose)
+    if(weapons.includes(playerSelectionClean)){
+        console.log('You choose a good Weapon! Let\'s fight!')
+        if (playerSelectionClean == 'Rock' && computerSelection == 'Scissors') {
+            console.log(playerWon);
+        }
+        else if (playerSelectionClean == 'Paper' && computerSelection == 'Rock') {
+            console.log(playerWon);
+        } 
+        else if (playerSelectionClean == 'Scissors' && computerSelection == 'Paper') {
+            console.log(playerWon);
+        } 
+        else if (playerSelectionClean === computerSelection){
+            console.log("It's a tie!")
+        }
+        else {
+            console.log(playerLose)
+        }
+    } else {
+        console.log(playerSelectionClean + " is not allowed as a weapon here...")
     }
 }
 
-function game() {
-    for (let i = 0; i < 5; i++){
-    playRound(playerSelectionClean, computerSelection);
-    }
-}
-
-game()
+playRound(playerSelectionClean, computerSelection)
